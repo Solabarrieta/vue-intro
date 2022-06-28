56285e626ad2c69348fa3965775d9a24e043fecd
oier <oieruni22@gmail.com>
2022-06-28 12:44:31 +0200

    Finalizando introducción a Vue.js

---
+++ progresive.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <p>Lorem ipsum</p>
+    <div>{{ text }}</div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "Hola"
+        };
+    }
+};
+</script>
+
+<style scoped>
+p {
+    color: turquoise;
+}
+</style>
